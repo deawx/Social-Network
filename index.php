c33b1f23fe222ffe6daaf88a6613e2e59bbe382e
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-21 10:46:14 +0100

    Add Navbar & Jquery

--- index.php
+++ index.php
@@ -21,9 +21,37 @@
 </head>
 
 <body>
+	<nav class="blue lighten-1">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Red Stone Shop</a>
+      <ul class="right hide-on-med-and-down">
+				<li>
+					<a class="dropdown-trigger" href="#!" data-target="dropdown">Menu
+						<i class="material-icons right">arrow_drop_down</i>
+					</a>
 
+					<ul id="dropdown" class="dropdown-content">
+  					<li><a href="#!">Shirts</a></li>
+  					<li><a href="#!">Pants</a></li>
+						<li><a href="#!">Shoes</a></li>
+						<li><a href="#!">Accessories</a></li>
+					</ul>
+				</li>
+        <li><a href=""><i class="material-icons right">search</i>Search </a></li>
+      </ul>
+    </div>
+  </nav>
 
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
   <script src="assets/js/materialize.min.js"></script>
+
+	<script>
+		$(document).ready(function() {
+			$(".dropdown-trigger").dropdown();
+		});
+	</script>
+
+        
 
 </body>
 </html>
