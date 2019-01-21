3883ce7bf828af03e46b8d99c8e670b73cb7a6f3
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-21 10:00:35 +0100

    Add Index Structure file

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="E-Commerce Website">
+	<meta name="author" content="Laurent Echeverria">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>E-Commerce</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/materialize.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/material-icons.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+</head>
+
+<body>
+
+
+  <script src="assets/js/materialize.min.js"></script>
+
+</body>
+</html>
