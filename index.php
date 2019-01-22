dc5ededcb9aa6a671f04dd2a65466bd4be8b3213
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-22 15:08:20 +0100

    Add grid for products

--- index.php
+++ index.php
@@ -64,7 +64,298 @@
 				</li>
 			</ul>
 		</div>
-  </nav>
+	</nav>
+	<div class="row">
+		<h2 class="featured">Featured Product</h2>
+</div>
+	<div class="row">
+    <div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 1</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 2</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 3</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 4</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 5</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 6</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 7</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 8</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 9</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 10</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 11</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart
+					</a>
+        </div>
+      </div>
+    </div>
+
+		<div class="col s12 m3">
+      <div class="card large">
+        <div class="card-image">
+          <img src="assets/img/item-1.jpg">
+          <span class="card-title">Item 12</span>
+        </div>
+
+				<div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+						 I am convenient because I require little markup to use effectively.
+						 I am convenient because I require little markup to use effectively.
+						 <br />
+					</p>
+				</div>
+
+        <div class="card-action">
+					<a class="waves-effect waves-light btn">
+						<i class="material-icons left">add_shopping_cart</i>
+						Add to Cart</a>
+        </div>
+      </div>
+    </div>
+	</div>
 
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
   <script src="assets/js/materialize.min.js"></script>
