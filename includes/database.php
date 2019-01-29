8d3a7239a324d15328f02486bd13819e996cfb19
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-29 17:48:52 +0100

    Update query & Add session_start, ob_start, timezone

--- includes/database.php
+++ includes/database.php
@@ -1,15 +1,19 @@
 <?php
 
-$Connection = mysqli_connect('localhost', 'root', '', 'social');
+ob_start();
+session_start();
 
-if(!$Connection) {
+$timezone = date_default_timezone_set("Europe/Paris");
+$con = mysqli_connect('localhost', 'root', '', 'social');
+
+if(!$con) {
     die('Database connection failed');
 };
 
-$Database = mysqli_select_db($Connection, 'social');
+$db = mysqli_select_db($con, 'social');
 
-if(!$Database) {
-    die('Database selection failed: ' . mysqli_connect_error($Connection));
+if(!$db) {
+    die('Database selection failed: ' . mysqli_connect_error($con));
 };
 
 ?>
