0d77c1eba3c9a15bc8bc2c7d7295abfbe23891ad
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-29 13:46:59 +0100

    Update globale style

--- register.php
+++ register.php
@@ -85,167 +85,142 @@ if(isset($_POST['reg_btn'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
 
-  <link rel="stylesheet" type="text/css" href="assets/css/fonts.css" />
-  <link rel="stylesheet" type="text/css" href="assets/css/material-kit.min.css" />
+  <link rel="stylesheet" type="text/css" href="../../../fonts.googleapis.com/cssff98.css?family=Open+Sans:300,400,600,700">
+  <link rel="stylesheet" type="text/css" href="assets/css/nucleo.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/argon.min.css" />
 
   <title>Material Social Network - Register</title>
 </head>
 
-<body>
-  <div class="page-header header-filter" style="background-image: url('assets/img/bg2.jpg'); background-size: cover; background-position: center center;">
-    <div class="container">
-      <div class="container-fluid" style="margin-top:60px;">
-        <form action="register.php" method="POST">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-              <div class="card card-login card-hidden">
-                <div class="card-header card-header-rose text-center">
-                  <h4 class="card-title">Register</h4>
-                </div>
-              
-                <div class="card-body ">
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">person_outline</i>
-                        </span>
-                      </div>
-                      <input type="text" id="reg_fname" name="reg_fname" class="form-control" placeholder="First Name"
-                             value="<?php if(isset($_SESSION['reg_fname'])) echo $_SESSION['reg_fname']; ?>"
-                      />
-
-                      <br />
-
-                      <p class="text-danger text-center">
-                        <?php 
-                          if(in_array("Your first name must be between 3 & 25 characters.", $error_array)) 
-                            echo "Your first name must be between 3 & 25 characters.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">person</i>
-                        </span>
-                      </div>
-                      <input type="text" id="reg_lname" name="reg_lname" class="form-control" placeholder="Last Name"
-                             value="<?php if(isset($_SESSION['reg_lname'])) echo $_SESSION['reg_lname']; ?>"
-                      />
-
-                      <br />
-
-                      <p class="text-danger text-center">
-                        <?php
-                          if(in_array("Your last name must be between 3 & 25 characters.", $error_array)) 
-                            echo "Your last name must be between 3 & 25 characters.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">mail_outline</i>
-                        </span>
-                      </div>
-                      <input type="email" id="reg_email" name="reg_email" class="form-control" placeholder="Email"
-                             value="<?php if(isset($_SESSION['reg_email'])) echo $_SESSION['reg_email']; ?>"
-                      />
-
-                      <br />
-
-                      <p class="text-danger text-center">
-                        <?php
-                          if (in_array("Invalid email format.", $error_array)) echo "Invalid email format.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">email</i>
-                        </span>
-                      </div>
-                      <input type="email" id="reg_email2" name="reg_email2" class="form-control" placeholder="Confirm Email"
-                             value="<?php if(isset($_SESSION['reg_email2'])) echo $_SESSION['reg_email2']; ?>"
-                      />
-
-                      <br />
-
-                      <p class="text-danger text-center">
-                        <?php 
-                          if(in_array("Email already in use.", $error_array)) echo "Email already in use.";
-                          elseif (in_array("Email don't match.", $error_array)) echo "Email don't match.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">lock_outline</i>
-                        </span>
-                      </div>
-                      <input type="password" id="reg_password" name="reg_password" class="form-control" placeholder="Password" />
-
-                      <br />
-                      
-                      <p class="text-danger text-center">
-                        <?php
-                          if (in_array("Your password must be between 5 & 30 characters.", $error_array)) echo "Your password must be between 5 & 30 characters.";
-                          elseif (in_array("Your password can only contain english characters or numbers.", $error_array)) echo "Your password can only contain english characters or numbers.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-
-                  <span class="bmd-form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="material-icons">lock</i>
-                        </span>
-                      </div>
-                      <input type="password" id="reg_password2" name="reg_password2" class="form-control" placeholder="Confirm Password" />
-
-                      <br />
-
-                      <p class="text-danger text-center">
-                        <?php 
-                          if(in_array("Your password don't match.", $error_array)) echo "Your password don't match.";
-                        ?>
-                      </p>
-                    </div>
-                  </span>
-                </div>
-                <br />
-                <div class="card-footer ml-auto mr-auto">
-                  <button type="submit" id="reg_btn" name="reg_btn" class="btn btn-rose">Register Now</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
+<body class="bg-default">
+  <div class="main-content">
+    <div class="header bg-gradient-primary py-7 py-lg-8">
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
       </div>
     </div>
-  </div>
+
+    <div class="container mt--8 pb-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-8">
+          <div class="card bg-secondary shadow border-0">
+            <div class="card-body px-lg-5 py-lg-5">
+              <form role="form" action="register.php" method="POST">
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="First Name" type="text" name="reg_fname"
+                           value="<?php if(isset($_SESSION['reg_fname'])) echo $_SESSION['reg_fname']; ?>" />
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php 
+                      if(in_array("Your first name must be between 3 & 25 characters.", $error_array)) 
+                        echo "Your first name must be between 3 & 25 characters.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Last Name" type="text" name="reg_lname"
+                           value="<?php if(isset($_SESSION['reg_lname'])) echo $_SESSION['reg_lname']; ?>" />
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php
+                      if(in_array("Your last name must be between 3 & 25 characters.", $error_array)) 
+                        echo "Your last name must be between 3 & 25 characters.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Email" type="email" name="reg_email"
+                           value="<?php if(isset($_SESSION['reg_email'])) echo $_SESSION['reg_email']; ?>" />
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php
+                      if (in_array("Invalid email format.", $error_array)) echo "Invalid email format.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Confirm Email" type="email" name="reg_email2" 
+                           value="<?php if(isset($_SESSION['reg_email2'])) echo $_SESSION['reg_email2']; ?>" />
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php 
+                      if(in_array("Email already in use.", $error_array)) echo "Email already in use.";
+                      elseif (in_array("Email don't match.", $error_array)) echo "Email don't match.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-key-25"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Password" type="password" name="reg_password" />
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php
+                      if (in_array("Your password must be between 5 & 30 characters.", $error_array)) echo "Your password must be between 5 & 30 characters.";
+                      elseif (in_array("Your password can only contain english characters or numbers.", $error_array)) echo "Your password can only contain english characters or numbers.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Confirm Password" type="password" name="reg_password2"/>
+                  </div>
+
+                  <p class="text-danger text-center">
+                    <?php 
+                      if(in_array("Your password don't match.", $error_array)) echo "Your password don't match.";
+                    ?>
+                  </p>
+                </div>
+
+                <div class="text-center">
+                  <button type="submit" name="reg_btn" class="btn btn-primary mt-4">Create my account</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>    
 
   <script type="text/javascript" src="assets/js/lib/jquery.min.js"></script>
-  <script type="text/javascript" src="assets/js/lib/popper.min.js"></script>
-  <script type="text/javascript" src="assets/js/lib/bootstrap-material-design.min.js"></script>
-  <script type="text/javascript" src="assets/js/material-kit.min.js"></script>
+  <script type="text/javascript" src="assets/js/lib/bootstrap.bundle.min.js"></script>
+  <script type="text/javascript" src="assets/js/argon.min.js"></script>
 
 </body>
 </html>
