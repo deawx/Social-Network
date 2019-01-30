4f13d3d96fd0a6d9e6cb4d7964190ed7870b3668
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-01-30 10:34:14 +0100

    Add reopen_account func() & query

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -14,11 +14,16 @@ if(isset($_POST['log_btn'])) {
       $username = $row['username'];
       $_SESSION['username'] = $username;
 
+      $user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE (email='$email') AND (user_closed='yes')");
+      if(mysqli_num_rows($user_closed_query) == 1) {
+         $reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE (email='$email')");
+      };
+
       header("Location: index.php");
       exit();
    } else {
       array_push($error_array, "<div class='alert alert-danger text-center' style='margin-bottom: 35px;'><strong>Error! Email or password was incorrect.</strong></div>");
-   }
-}
+   };
+};
 
 ?>
