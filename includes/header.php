d0efce9f68797ba856dbc1f53930070b5b8c13eb
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-03 10:22:38 +0100

    Update Logout navlink

--- includes/header.php
+++ includes/header.php
@@ -98,7 +98,7 @@ if(isset($_SESSION['username'])) {
 
               <div class="dropdown-divider"></div>
 
-              <a href="#!" class="dropdown-item">
+              <a href="includes/handlers/logout.php" class="dropdown-item">
                 <i class="fas fa-sign-out-alt"></i>
                 <span>Logout</span>
               </a>
