a3e2c304775a11068167b2f7e88ba65f9dd7e082
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-05 17:38:21 +0100

    Add limit post loaded & msg if all post was loaded

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -38,144 +38,177 @@ class Post {
       }
    }
 
-   public function loadPostsFriends() {
+   public function loadPostsFriends($data, $limit) {
+      $page = $data['page'];
+      $userLoggedIn = $this->user_obj->getUsername();
+
+      if($page == 1) {
+         $start = 0;
+      } else {
+         $start = ($page - 1) * $limit;
+      }
+
       $str = "";
-      $data = mysqli_query($this->con, "SELECT * FROM posts WHERE (deleted='no') ORDER BY id DESC");
+      $data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE (deleted='no') ORDER BY id DESC");
+
+      if(mysqli_num_rows($data_query) > 0) {
+         $num_iterations = 0;
+         $count = 1;
    
-      while($row = mysqli_fetch_array($data)) {
-         $id = $row['id'];
-         $body = $row['body'];
-         $added_by = $row['added_by'];
-         $date_time = $row['date_added'];
+         while($row = mysqli_fetch_array($data_query)) {
+            $id = $row['id'];
+            $body = $row['body'];
+            $added_by = $row['added_by'];
+            $date_time = $row['date_added'];
 
-         // PREPARE user_to STRING SO IT CAN BE INCLUDED EVEN IF NOT POSTED TO A USER
-         if($row['user_to'] == "none") {
-            $user_to = "";
-         } else {
-            $user_to_obj = new User($this->con, $row['user_to']);
-            $user_to_name = $user_to_obj->getFirstAndLastName();
-            $user_to = "to <a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>";
-         }
-
-         // CHECK IF USER WHO POSTED, HAS THEIR ACCOUNT CLOSED
-         $added_by_obj = new User($this->con, $added_by);
-         if($added_by_obj->isClosed()) {
-            continue;
-         }
-
-         $user_infos = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE (username='$added_by')");
-         $user_row = mysqli_fetch_array($user_infos);
-
-         $first_name = $user_row['first_name'];
-         $last_name = $user_row['last_name'];
-         $profile_pic = $user_row['profile_pic'];
-
-         //TIMEFRAME
-         $date_time_now = date("Y-m-d  H:i:s");
-         $start_date = new DateTime($date_time);
-         $end_date = new DateTime($date_time_now);
-         $interval = $start_date->diff($end_date);
-
-         if($interval->y >= 1) {
-            if($interval == 1) {
-               $time_message = $interval->y . " year ago"; // 1 year
+            // PREPARE user_to STRING SO IT CAN BE INCLUDED EVEN IF NOT POSTED TO A USER
+            if($row['user_to'] == "none") {
+               $user_to = "";
             } else {
-               $time_message = $interval->y . " years ago"; // 1+ years
-            }
-         } elseif($interval->m >= 1) {
-            if($interval->d == 0) {
-               $days = " ago";
-            } elseif($interval->d == 1) {
-               $days = $interval->d . " day ago";
-            } else {
-               $days = $interval->d . " days ago";
+               $user_to_obj = new User($this->con, $row['user_to']);
+               $user_to_name = $user_to_obj->getFirstAndLastName();
+               $user_to = "to <a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>";
             }
 
-            if($interval->m == 1) {
-               $time_message = $interval->m . " month" . $days;
-            } else {
-               $time_message = $interval->m . " months" . $days;
+            // CHECK IF USER WHO POSTED, HAS THEIR ACCOUNT CLOSED
+            $added_by_obj = new User($this->con, $added_by);
+            if($added_by_obj->isClosed()) {
+               continue;
             }
-         } elseif($interval->d >= 1) {
-            if($interval->d == 1) {
-               $time_message = "Yesterday";
-            } else {
-               $time_message = $interval->d . " days ago";
-            }
-         } elseif($interval->h >= 1) {
-            if($interval->h == 1) {
-               $time_message = $interval->h . " hour ago";
-            } else {
-               $time_message = $interval->h . " hours ago";
-            }
-         } elseif($interval->i >= 1) {
-            if($interval->i == 1) {
-               $time_message = $interval->i . " minute ago";
-            } else {
-               $time_message = $interval->i . " minutes ago";
-            }
-         } else {
-            if($interval->s < 30) {
-               $time_message = "Just now";
-            } else {
-               $time_message = $interval->s . " seconds ago";
-            }
-         }
 
-         $str .= "
-            <div class='card-body border-0'>
-               <div class='p-3'>
-                  <div class='row align-items-center'>
-                     <div class='col-lg-2 ml-1 mr-1'>
-                        <img class='img-fluid rounded-circle shadow-lg' src='$profile_pic' />
-                     </div>
+            if($num_iterations++ < $start) {
+               continue;
+            }
 
-                     <div class='col-lg-8'>
-                        <h3 class='heading mb-0'>
-                           <a href='$added_by'>$first_name $last_name</a> $user_to
-                        </h3>
-                        <p class='mb-0 mt-3'>
-                           $body
-                        </p>
+            // ONCE $limit POSTS HAVE BEEN LOADED, BREAK
+            if($count > $limit) {
+               break;
+            } else {
+               $count++;
+            }
+
+            $user_infos = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE (username='$added_by')");
+            $user_row = mysqli_fetch_array($user_infos);
+
+            $first_name = $user_row['first_name'];
+            $last_name = $user_row['last_name'];
+            $profile_pic = $user_row['profile_pic'];
+
+            //TIMEFRAME
+            $date_time_now = date("Y-m-d  H:i:s");
+            $start_date = new DateTime($date_time);
+            $end_date = new DateTime($date_time_now);
+            $interval = $start_date->diff($end_date);
+
+            if($interval->y >= 1) {
+               if($interval == 1) {
+                  $time_message = $interval->y . " year ago"; // 1 year
+               } else {
+                  $time_message = $interval->y . " years ago"; // 1+ years
+               }
+            } elseif($interval->m >= 1) {
+               if($interval->d == 0) {
+                  $days = " ago";
+               } elseif($interval->d == 1) {
+                  $days = $interval->d . " day ago";
+               } else {
+                  $days = $interval->d . " days ago";
+               }
+
+               if($interval->m == 1) {
+                  $time_message = $interval->m . " month" . $days;
+               } else {
+                  $time_message = $interval->m . " months" . $days;
+               }
+            } elseif($interval->d >= 1) {
+               if($interval->d == 1) {
+                  $time_message = "Yesterday";
+               } else {
+                  $time_message = $interval->d . " days ago";
+               }
+            } elseif($interval->h >= 1) {
+               if($interval->h == 1) {
+                  $time_message = $interval->h . " hour ago";
+               } else {
+                  $time_message = $interval->h . " hours ago";
+               }
+            } elseif($interval->i >= 1) {
+               if($interval->i == 1) {
+                  $time_message = $interval->i . " minute ago";
+               } else {
+                  $time_message = $interval->i . " minutes ago";
+               }
+            } else {
+               if($interval->s < 30) {
+                  $time_message = "Just now";
+               } else {
+                  $time_message = $interval->s . " seconds ago";
+               }
+            }
+
+            $str .= "
+               <div class='card-body border-0'>
+                  <div class='p-3'>
+                     <div class='row align-items-center'>
+                        <div class='col-lg-2 ml-1 mr-1'>
+                           <img class='img-fluid rounded-circle shadow-lg' src='$profile_pic' />
+                        </div>
+
+                        <div class='col-lg-8'>
+                           <h3 class='heading mb-0'>
+                              <a href='$added_by'>$first_name $last_name</a> $user_to
+                           </h3>
+                           <p class='mb-0 mt-3'>
+                              $body
+                           </p>
+                        </div>
                      </div>
                   </div>
                </div>
-            </div>
 
-            <div class='card-footer bg-secondary border-0'>
-               <div class='row'>
-                  <div class='col-lg-3 text-left'>
-                     <button type='submit' class='btn btn-primary btn-icon'>
-                        <span class='btn-inner--icon'>
-                           <i class='fas fa-calendar-day' style='font-size: 20px;'></i>
-                        </span>
+               <div class='card-footer bg-secondary border-0'>
+                  <div class='row'>
+                     <div class='col-lg-3 text-left'>
+                        <button type='submit' class='btn btn-primary btn-icon'>
+                           <span class='btn-inner--icon'>
+                              <i class='fas fa-calendar-day' style='font-size: 20px;'></i>
+                           </span>
 
-                        <span class='btn-inner--text'>$time_message</span>
-                     </button>
-                  </div>
-  
-                  <div class='col-lg-9 text-right'>
-                     <button type='submit' class='btn btn-outline-danger btn-icon'>
-                        <span class='btn-inner--icon'>
-                           <i class='fas fa-heart' style='font-size: 16px;'></i>
-                        </span>
+                           <span class='btn-inner--text'>$time_message</span>
+                        </button>
+                     </div>
+   
+                     <div class='col-lg-9 text-right'>
+                        <button type='submit' class='btn btn-outline-danger btn-icon'>
+                           <span class='btn-inner--icon'>
+                              <i class='fas fa-heart' style='font-size: 16px;'></i>
+                           </span>
 
-                        <span class='btn-inner--text'>18</span>
-                     </button>
-  
-                     <button type='submit' class='btn btn-outline-info btn-icon'>
-                        <span class='btn-inner--icon'>
-                           <i class='fas fa-comments' style='font-size: 16px;'></i>
-                        </span>
+                           <span class='btn-inner--text'>18</span>
+                        </button>
+   
+                        <button type='submit' class='btn btn-outline-info btn-icon'>
+                           <span class='btn-inner--icon'>
+                              <i class='fas fa-comments' style='font-size: 16px;'></i>
+                           </span>
 
-                        <span class='btn-inner--text'>10</span>
-                     </button>
+                           <span class='btn-inner--text'>10</span>
+                        </button>
+                     </div>
                   </div>
                </div>
-            </div>
 
-            <hr class='my-4' />
-         "; // END $str
+               <hr class='my-4' />
+            "; // END $str
+         }
+
+         if($count > $limit) {
+            $str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
+                     <input type='hidden' class='noMorePosts' value='false'>";
+         } else {
+            $str .= "<input type='hidden' class='noMorePosts' value='true'>
+                     <p>No more posts to show</p>";
+         }
       }
 
       echo $str;
