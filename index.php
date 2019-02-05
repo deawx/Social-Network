2157c2be43693bd8de8719962df5eeaefc6496b3
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-05 17:37:06 +0100

    Update script & Ajax request for infinite scroll

--- index.php
+++ index.php
@@ -139,35 +139,81 @@ if(isset($_POST['post_btn'])) {
           </div>
         </div>
 
-        <?php 
-          $post = new Post($con, $userLoggedIn);
-          $post->loadPostsFriends();
-        ?>
-
+        <div class="posts_area"></div>
         <img id="loading" src="assets/img/icons/loading.gif" alt="loader" style="max-width: 100px;" />
+
       </div>
     </div>
   </div>
 
-  <script>
-    var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-    $(document).ready(function() {
-      $('#loading').show();
-
-      // AJAX REQUEST FOR LOADING FIRST POSTS
-      $.ajax({
-        url: "includes/handlers/ajax_load_posts.php",
-        type: "POST",
-        data: "page=1&userLoggedIn=" + userLoggedIn,
-        cache: false
-      })
-    });
-  </script>
 
   <script type="text/javascript" src="assets/js/lib/jquery.min.js"></script>
   <script type="text/javascript" src="assets/js/lib/bootstrap.bundle.min.js"></script>
   <script type="text/javascript" src="assets/js/argon.min.js"></script>
+
+  <script>
+    $(function() { 
+      var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+      var inProgress = false;
+
+      loadPosts();
+
+      $(window).scroll(function() {
+        var bottomElement = $(".status_post").last();
+        var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+        if(isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+          loadPosts();
+        }
+      });
+
+      function loadPosts() {
+        if(inProgress) {
+          return;
+        }
+
+        inProgress = true;
+        $('#loading').show();
+
+        var page = $('.posts_area').find('.nextPage').val() || 1;
+
+        $.ajax({
+          url: "includes/handlers/ajax_load_posts.php",
+          type: "POST",
+          data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+          cache: false,
+
+          success: function(response) {
+            $('.posts_area').find('.nextPage').remove();
+            $('.posts_area').find('.noMorePosts').remove();
+            $('.posts_area').find('.noMorePostsText').remove();
+
+            $('#loading').hide();
+            $(".posts_area").append(response);
+
+            inProgress = false;
+          }
+        });
+      }
+
+      //CHECK IF THE ELEMENT IS IN VIEW
+      function isElementInView (element) {
+        if(element == null) {
+          return;
+        }
+
+        var rect = element.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+    });
+
+  </script>
 
 </body>
 </html>
