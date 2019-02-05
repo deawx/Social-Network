af43f845108edbb2887c7a7ba30d7ffa4f7f9a09
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-05 17:39:01 +0100

    Fix $_REQUEST

--- includes/handlers/ajax_load_posts.php
+++ includes/handlers/ajax_load_posts.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once("../../config/config.php");
-require_once("../classes/Post.php");
 require_once("../classes/User.php");
+require_once("../classes/Post.php");
 
 $limit = 10;
 
-$posts = new Post($con, $_REQUEST['$userLoggedIn']);
-$posts->loadPostsFriends();
+$posts = new Post($con, $_REQUEST['userLoggedIn']);
+$posts->loadPostsFriends($_REQUEST, $limit);
 
 ?>
