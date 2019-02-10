ca0326daabdc42ce0d22033930c538af88dee8c4
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-10 10:57:18 +0100

    Add didReceiveRequest() func

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -1,62 +1,73 @@
 <?php
 
 class User {
-   private $user;
-   private $con;
+  private $user;
+  private $con;
 
-   public function __construct($con, $user) {
-      $this->con = $con;
+  public function __construct($con, $user) {
+    $this->con = $con;
 
-      $user_infos = mysqli_query($con, "SELECT * FROM users WHERE (username='$user')");
-      $this->user = mysqli_fetch_array($user_infos); 
-   }
+    $user_infos = mysqli_query($con, "SELECT * FROM users WHERE (username='$user')");
+    $this->user = mysqli_fetch_array($user_infos); 
+  }
 
-   public function getUsername() {
-      return $this->user['username'];
-   }
+  public function getUsername() {
+    return $this->user['username'];
+  }
 
-   public function getNumPosts() {
-      $username = $this->user['username'];
-      $query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE (username='$username')");
-      $row = mysqli_fetch_array($query);
-      return $row['num_posts'];
-   }
+  public function getNumPosts() {
+    $username = $this->user['username'];
+    $query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE (username='$username')");
+    $row = mysqli_fetch_array($query);
+    return $row['num_posts'];
+  }
 
-   public function getFirstAndLastName() {
-      $username = $this->user['username'];
-      $query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE (username='$username')");
-      $row = mysqli_fetch_array($query);
-      return $row['first_name'] . " " . $row['last_name'];
-   }
+  public function getFirstAndLastName() {
+    $username = $this->user['username'];
+    $query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE (username='$username')");
+    $row = mysqli_fetch_array($query);
+    return $row['first_name'] . " " . $row['last_name'];
+  }
 
-   public function getProfilePic() {
-      $username = $this->user['username'];
-      $query = mysqli_query($this->con, "SELECT profile_pic FROM users WHERE (username='$username')");
-      $row = mysqli_fetch_array($query);
-      return $row['profile_pic'];
-   }
+  public function getProfilePic() {
+    $username = $this->user['username'];
+    $query = mysqli_query($this->con, "SELECT profile_pic FROM users WHERE (username='$username')");
+    $row = mysqli_fetch_array($query);
+    return $row['profile_pic'];
+  }
 
-   public function isClosed() {
-      $username = $this->user['username'];
-      $query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE (username='$username')");
-      $row = mysqli_fetch_array($query);
+  public function isClosed() {
+    $username = $this->user['username'];
+    $query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE (username='$username')");
+    $row = mysqli_fetch_array($query);
 
-      if($row['user_closed'] == 'yes') {
-         return true;
-      } else {
-         return false;
-      }
-   }
+    if($row['user_closed'] == 'yes') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-   public function isFriend($username_to_check) {
-      $usernameComma = "," . $username_to_check . ",";
+  public function isFriend($username_to_check) {
+    $usernameComma = "," . $username_to_check . ",";
 
-      if((strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username'])) {
-         return true;
-      } else {
-         return false;
-      }
-   }
+    if((strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username'])) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function didReceiveRequest($user_to) {
+    $user_from = $this->user['username'];
+    $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE (user_to='$user_to' AND user_from='$user_from')");
+
+    if(mysqli_num_rows($check_request_query) > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 ?>
