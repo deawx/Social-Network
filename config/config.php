fff143fc445bb72354a4bd424a376f292b945d12
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-11 09:33:33 +0100

    Fix spacing tab

--- config/config.php
+++ config/config.php
@@ -7,13 +7,13 @@ $timezone = date_default_timezone_set("Europe/Paris");
 $con = mysqli_connect('localhost', 'root', '', 'social');
 
 if(!$con) {
-    die('Database connection failed');
+  die('Database connection failed');
 };
 
 $db = mysqli_select_db($con, 'social');
 
 if(!$db) {
-    die('Database selection failed: ' . mysqli_connect_error($con));
+  die('Database selection failed: ' . mysqli_connect_error($con));
 };
 
 ?>
