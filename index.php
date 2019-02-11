150d9b85be9d630c54b6226328ce6dffb17396d8
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-11 10:11:28 +0100

    Delete Import Post -> is in header now

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 include_once("includes/header.php");
-include_once("includes/classes/Post.php");
 
 if(isset($_POST['post_btn'])) {
   $post = new POST($con, $userLoggedIn);
@@ -148,67 +147,65 @@ if(isset($_POST['post_btn'])) {
     </div>
   </div>
 
-<script>
-$(document).ready(function() {
-  var userLoggedIn = '<?php echo $userLoggedIn; ?>';
-  var inProgress = false;
-
-  loadPosts();
-
-  $(window).scroll(function() {
-    var bottomElement = $(".status_post").last();
-    var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-
-    if(isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+  <script>
+    $(document).ready(function() {
+      var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+      var inProgress = false;
       loadPosts();
-    }
-  });
 
-  function loadPosts() {
-    if(inProgress) {
-      return;
-    }
+      $(window).scroll(function() {
+        var bottomElement = $(".status_post").last();
+        var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+        if(isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+          loadPosts();
+        }
+      });
+
+      function loadPosts() {
+        if(inProgress) {
+          return;
+        }
  
-    inProgress = true;
-    $('#loading').show();
- 
-    var page = $('.posts_area').find('.nextPage').val() || 1;
+        inProgress = true;
+        $('#loading').show();
+        var page = $('.posts_area').find('.nextPage').val() || 1;
 
-    $.ajax({
-      url: "includes/handlers/ajax_load_posts.php",
-      type: "POST",
-      data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-      cache: false,
+        $.ajax({
+          url: "includes/handlers/ajax_load_posts.php",
+          type: "POST",
+          data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+          cache: false,
 
-      success: function(response) {
-        $('.posts_area').find('.nextPage').remove();
-        $('.posts_area').find('.noMorePosts').remove();
-        $('.posts_area').find('.noMorePostsText').remove();
+          success: function(response) {
+            $('.posts_area').find('.nextPage').remove();
+            $('.posts_area').find('.noMorePosts').remove();
+            $('.posts_area').find('.noMorePostsText').remove();
 
-        $('#loading').hide();
-        $(".posts_area").append(response);
+            $('#loading').hide();
+            $(".posts_area").append(response);
 
-        inProgress = false;
+            inProgress = false;
+          }
+        });
+      }
+
+      //CHECK IF THE ELEMENT IS IN VIEW
+      function isElementInView(element) {
+        if(element == null) {
+          return;
+        }
+
+        var rect = element.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
       }
     });
-  }
 
-  //CHECK IF THE ELEMENT IS IN VIEW
-  function isElementInView(element) {
-    if(element == null) {
-      return;
-    }
-
-    var rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-});
-</script>
-
+  </script>
 </body>
 </html>
