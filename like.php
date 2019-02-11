29b9a366c73fba577e0dd8d18ee1e5ec69016ad7
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-11 09:13:54 +0100

    Fix close tag </html>

--- like.php
+++ like.php
@@ -32,78 +32,74 @@ if(isset($_SESSION['username'])) {
 </head>
 
 <body>
+  <?php
 
-   <?php
+  // GET ID OF POST
+  if(isset($_GET['post_id'])) {
+    $post_id = $_GET['post_id'];
+  }
 
-   // GET ID OF POST
-   if(isset($_GET['post_id'])) {
-      $post_id = $_GET['post_id'];
-   }
+  $get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE (id='$post_id')");
+  $row = mysqli_fetch_array($get_likes);
+  $total_likes = $row['likes'];
+  $user_liked = $row['added_by'];
 
-   $get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE (id='$post_id')");
-   $row = mysqli_fetch_array($get_likes);
-   $total_likes = $row['likes'];
-   $user_liked = $row['added_by'];
+  $user_infos = mysqli_query($con, "SELECT * FROM users WHERE (username='$user_liked')");
+  $row = mysqli_fetch_array($user_infos);
+  $total_users_likes = $row['num_likes'];
 
-   $user_infos = mysqli_query($con, "SELECT * FROM users WHERE (username='$user_liked')");
-   $row = mysqli_fetch_array($user_infos);
-   $total_users_likes = $row['num_likes'];
+  // LIKE BTN
+  if(isset($_POST['like_btn'])) {
+    $total_likes++;
+    $likes_query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE (id='$post_id')");
 
-   // LIKE BTN
-   if(isset($_POST['like_btn'])) {
-      $total_likes++;
-      $likes_query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE (id='$post_id')");
+    $total_users_likes++;
+    $users_likes_query = mysqli_query($con, "UPDATE users SET num_likes='$total_users_likes' WHERE (username='$user_liked')");
 
-      $total_users_likes++;
-      $users_likes_query = mysqli_query($con, "UPDATE users SET num_likes='$total_users_likes' WHERE (username='$user_liked')");
+    $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('', '$userLoggedIn', '$post_id')");
 
-      $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('', '$userLoggedIn', '$post_id')");
+    // INSERT NOTIFICATION
+  }
 
-      // INSERT NOTIFICATION
-   }
+  // UNLIKE BTN
+  if(isset($_POST['unlike_btn'])) {
+    $total_likes--;
+    $likes_query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE (id='$post_id')");
 
-   // UNLIKE BTN
-   if(isset($_POST['unlike_btn'])) {
-      $total_likes--;
-      $likes_query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE (id='$post_id')");
+    $total_users_likes--;
+    $users_likes_query = mysqli_query($con, "UPDATE users SET num_likes='$total_users_likes' WHERE (username='$user_liked')");
 
-      $total_users_likes--;
-      $users_likes_query = mysqli_query($con, "UPDATE users SET num_likes='$total_users_likes' WHERE (username='$user_liked')");
+    $insert_user = mysqli_query($con, "DELETE FROM likes WHERE (username='$userLoggedIn' AND post_id='$post_id')");
+  }
 
-      $insert_user = mysqli_query($con, "DELETE FROM likes WHERE (username='$userLoggedIn' AND post_id='$post_id')");
+  // CHECK FOR PREVIOUS LIKE
+  $check_query = mysqli_query($con, "SELECT * FROM likes WHERE (username='$userLoggedIn' AND post_id='$post_id')");
+  $num_rows = mysqli_num_rows($check_query);
 
-   }
+  if($num_rows > 0) {
+    echo "
+      <form action='like.php?post_id=" . $post_id . "' method='POST'>
+        <button type='submit' name='unlike_btn' class='btn btn-danger btn-icon'>
+          <span class='btn-inner--icon'>
+            <i class='fas fa-heart' style='font-size: 18px;'></i>
+          </span>
+          <span class='btn-inner--text'>$total_likes Likes</span>
+        </button>
+      </form>
+    ";
+  } else {
+    echo "
+      <form action='like.php?post_id=" . $post_id . "' method='POST'>
+        <button type='submit' name='like_btn' class='btn btn-outline-danger btn-icon'>
+          <span class='btn-inner--icon'>
+            <i class='fas fa-heart' style='font-size: 18px;'></i>
+          </span>
+          <span class='btn-inner--text'>$total_likes Likes</span>
+        </button>
+      </form>
+    ";
+  }
 
-   // CHECK FOR PREVIOUS LIKE
-   $check_query = mysqli_query($con, "SELECT * FROM likes WHERE (username='$userLoggedIn' AND post_id='$post_id')");
-   $num_rows = mysqli_num_rows($check_query);
-
-   if($num_rows > 0) {
-      echo "
-         <form action='like.php?post_id=" . $post_id . "' method='POST'>
-            <button type='submit' name='unlike_btn' class='btn btn-danger btn-icon'>
-               <span class='btn-inner--icon'>
-                  <i class='fas fa-heart' style='font-size: 18px;'></i>
-               </span>
-
-               <span class='btn-inner--text'>$total_likes Likes</span>
-            </button>
-         </form>
-      ";
-   } else {
-      echo "
-         <form action='like.php?post_id=" . $post_id . "' method='POST'>
-            <button type='submit' name='like_btn' class='btn btn-outline-danger btn-icon'>
-               <span class='btn-inner--icon'>
-                  <i class='fas fa-heart' style='font-size: 18px;'></i>
-               </span>
-
-               <span class='btn-inner--text'>$total_likes Likes</span>
-            </button>
-         </form>
-      ";
-   }
-
-   ?>
-
+  ?>
 </body>
+</html>
