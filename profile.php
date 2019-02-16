f2b6db04e774baddadcf6c10705a0da490a670fa
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-16 10:40:53 +0100

    Update Profile Style

--- profile.php
+++ profile.php
@@ -1,40 +1,34 @@
 <?php
 include_once("includes/header.php");
-
 if(isset($_GET['profile_username'])) {
    $username = $_GET['profile_username'];
    $user_infos = mysqli_query($con, "SELECT * FROM users WHERE (username='$username')");
    $user = mysqli_fetch_array($user_infos);
    $num_friends = (substr_count($user['friend_array'], ",")) - 1;
 }
-
 if(isset($_POST['remove_friend'])) {
   $user = new User($con, $userLoggedIn);
   $user->removeFriend($username);
   header("Location: $username");
 }
-
 if(isset($_POST['add_friend'])) {
   $user = new User($con, $userLoggedIn);
   $user->sendRequest($username);
   header("Location: $username");
 }
-
 if(isset($_POST['respond_request'])) {
   header("Location: requests.php");
 }
-
 if(isset($_POST['post_btn'])) {
   $post = new Post($con, $_POST['user_from']);
   $post->submitPost($_POST['post_body'], $_POST['user_to']);
 }
-
 ?>
 
 <div class="header bg-gradient-primary pb-8 pt-5 pt-md-9">
   <div class="container-fluid mt--3">
     <div class="row">
-      <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
+      <div class="col-xl-5 order-xl-2 mb-5 mb-xl-0">
         <div class="card card-profile shadow">
           <div class="row justify-content-center">
             <div class="col-lg-3 order-lg-2">
@@ -48,15 +42,70 @@ if(isset($_POST['post_btn'])) {
 
           <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
             <div class="d-flex justify-content-between">
-              <a href="#" class="btn btn-sm btn-info mr-4">Connect</a>
-              <a href="#" class="btn btn-sm btn-default float-right">Message</a>
+              <form action="<?php echo $username; ?>" method="POST">
+                <?php
+                  $profile_user_obj = new User($con, $username);
+                  if($profile_user_obj->isClosed()) {
+                    header("Location: user_closed.php");
+                  }
+
+                  $logged_in_user_obj = new User($con, $userLoggedIn);
+                  if($userLoggedIn != $username) {
+                    if($logged_in_user_obj->isFriend($username)) {
+                      echo "
+                        <button type='submit' name='remove_friend' class='btn btn-sm btn-danger btn-icon'>
+                          <span class='btn-inner--icon'>
+                            <i class='fas fa-user-minus' style='font-size: 15px;'></i>
+                          </span>
+                          <span class='btn-inner--text'>REMOVE FRIEND</span>
+                        </button>
+                      "; 
+                    } elseif($logged_in_user_obj->didReceiveRequest($username)) {
+                      echo "
+                        <button type='submit' name='respond_request' class='btn btn-sm btn-warning btn-icon'>
+                          <span class='btn-inner--icon'>
+                            <i class='fas fa-user-clock' style='font-size: 15px;'></i>
+                          </span>
+                          <span class='btn-inner--text'>RESPOND TO REQUEST</span>
+                        </button>
+                      ";
+                    } elseif($logged_in_user_obj->didSendRequest($username)) {
+                      echo "
+                        <button type='submit' name='' class='btn btn-neutral btn-icon'>
+                          <span class='btn-inner--icon'>
+                            <i class='fas fa-handshake' style='font-size: 15px;'></i>
+                          </span>
+                          <span class='btn-inner--text'>REQUEST SENT</span>
+                        </button>
+                      ";
+                    } else {
+                      echo "
+                        <button type='submit' name='add_friend' class='btn btn-sm btn-primary btn-icon'>
+                          <span class='btn-inner--icon'>
+                            <i class='fas fa-user-plus' style='font-size: 15px;'></i>
+                          </span>
+                          <span class='btn-inner--text'>ADD FRIEND</span>
+                        </button>
+                      ";
+                    }
+                  }
+                ?>
+              </form>
+
+              <button type='' name='' class='btn btn-sm btn-primary btn-icon'>
+                <span class='btn-inner--icon'>
+                  <i class='fas fa-paper-plane' style='font-size: 15px;'></i>
+                </span>
+                <span class='btn-inner--text'>PRIVATE MESSAGE</span>
+              </button>
+
             </div>
           </div>
 
           <div class="card-body pt-0 pt-md-4">
             <div class="row">
               <div class="col">
-                <div class="card-profile-stats d-flex justify-content-center mt-md-5">
+                <div class="card-profile-stats d-flex justify-content-center md-5">
                   <div>
                     <span class="heading"><?php echo $num_friends; ?></span>
                     <span class="description">Friends</span>
@@ -96,102 +145,37 @@ if(isset($_POST['post_btn'])) {
         </div>
       </div>
 
-      <div class="col-xl-8 order-xl-1">
+      <div class="col-xl-7 order-xl-1">
         <div class="card bg-secondary shadow">
-          <form action="<?php echo $username; ?>" method="POST">
+          <form method="POST" action="<?php echo $username; ?>">
             <div class="card-header bg-white border-0">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3 class="mb-0">
-                    Profile of <?php echo $user_obj->getFirstAndLastName(); ?>
+                  <h3 class="mb-0 heading">
+                    Post a message to your friend
                   </h3>
-                </div>
-
-                <div class="col-4 text-right">
-                  <?php
-                    $profile_user_obj = new User($con, $username);
-                    if($profile_user_obj->isClosed()) {
-                      header("Location: user_closed.php");
-                    }
-
-                    $logged_in_user_obj = new User($con, $userLoggedIn);
-                    if($userLoggedIn != $username) {
-                      if($logged_in_user_obj->isFriend($username)) {
-                        echo "
-                          <button type='submit' name='remove_friend' class='btn btn-danger btn-icon'>
-                            <span class='btn-inner--icon'>
-                              <i class='fas fa-user-minus' style='font-size: 20px;'></i>
-                            </span>
-                            <span class='btn-inner--text'> Remove Friend</span>
-                          </button>
-                        "; 
-                      } elseif($logged_in_user_obj->didReceiveRequest($username)) {
-                        echo "
-                          <button type='submit' name='respond_request' class='btn btn-warning btn-icon'>
-                            <span class='btn-inner--icon'>
-                              <i class='fas fa-user-clock' style='font-size: 20px;'></i>
-                            </span>
-                            <span class='btn-inner--text'> Respond to Request</span>
-                          </button>
-                        ";
-                      } elseif($logged_in_user_obj->didSendRequest($username)) {
-                        echo "
-                          <button type='submit' name='' class='btn btn-neutral btn-icon'>
-                            <span class='btn-inner--icon'>
-                              <i class='fas fa-handshake' style='font-size: 20px;'></i>
-                            </span>
-                            <span class='btn-inner--text'> Request Sent</span>
-                          </button>
-                        ";
-                      } else {
-                        echo "
-                          <button type='submit' name='add_friend' class='btn btn-primary btn-icon'>
-                            <span class='btn-inner--icon'>
-                              <i class='fas fa-user-plus' style='font-size: 20px;'></i>
-                            </span>
-                            <span class='btn-inner--text'> Add Friend</span>
-                          </button>
-                        ";
-                      }
-                    }
-                  ?>
-
                 </div>
               </div>
             </div>
+
+            <div class="card-body">
+              <div class="form-group">
+                <textarea rows="6" name="post_body" class="form-control form-control-alternative mt-1">What do you want to tell him?</textarea>
+              </div>
+
+              <input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>" />
+              <input type="hidden" name="user_to" value="<?php echo $username; ?>" />
+
+              <div class="text-right">
+                <button type="submit" name="post_btn" id="post_btn" class="btn btn-primary btn-icon  mt-1">
+                  <span class="btn-inner--icon">
+                    <i class="fas fa-paper-plane"></i>
+                  </span>
+                  <span class="btn-inner--text">Send Post</span>
+                </button>
+              </div>
+            </div>
           </form>
-
-          <div class="card bg-secondary shadow">
-            <form method="POST" action="<?php echo $username; ?>">
-              <div class="card-body border-0">
-                <div class="row align-items-center">
-                  <div class="col-8">
-                    <h6 class="heading-small ml-4 mb--4">Post a message to your friend</h6>
-                  </div>
-                </div>
-              </div>
-
-              <div class="card-body">
-                <div class="pl-lg-4">
-                  <div class="form-group">
-                    <textarea rows="4" name="post_body" class="form-control form-control-alternative">What do you want to tell him?</textarea>
-                  </div>
-                </div>
-
-                <input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>" />
-                <input type="hidden" name="user_to" value="<?php echo $username; ?>" />
-
-                <div class="text-right">
-                  <button type="submit" name="post_btn" id="post_btn" class="btn btn-primary btn-icon mb-3 mb-sm-0">
-                    <span class="btn-inner--icon">
-                      <i class="fas fa-paper-plane"></i>
-                    </span>
-                    <span class="btn-inner--text">Send ...</span>
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
         </div>
       </div>
     </div>
