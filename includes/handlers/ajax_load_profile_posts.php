f5d1b11a33ba8d2b652946f61042ceaa16d75d6a
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-17 11:00:17 +0100

    Request for load friends posts

---
+++ includes/handlers/ajax_load_profile_posts.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("../../config/config.php");
+include_once("../classes/User.php");
+include_once("../classes/Post.php");
+
+$limit = 10;
+
+$posts = new Post($con, $_REQUEST['userLoggedIn']);
+$posts->loadProfilePosts($_REQUEST, $limit);
+
+?>
