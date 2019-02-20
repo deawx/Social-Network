a1cf51d807e1c9e106113032861e21f9f0715697
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-20 16:10:19 +0100

    Add Style container for Convers List

--- messages.php
+++ messages.php
@@ -27,112 +27,73 @@ if(isset($_POST['post_msg'])) {
 ?>
 
   <div class="header bg-gradient-primary pb-8 pt-5 pt-md-9">
-    <div class="container">
-      <div class="header-body">
-        <div class="row">
-    
-          <div class="col-xl-4 col-lg-6">
-            <div class="card card-stats mb-4 mb-xl-0">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Friends</h5>
-                    <span class="h2 font-weight-bold mb-0">
-                      <?php echo $num_friends; ?>
-                    </span>
-                  </div>
-                  
-                  <div class="col-auto">
-                    <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                      <i class="fas fa-user-friends"></i>
-                    </div>
-                  </div>
+    <div class="container-fluid mt--3">
+      <div class="row">
+        <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
+          <div class="card card-profile shadow">
+            <div class='card-header bg-white border-0'>
+              <div class='row align-items-center'>
+                <div class='col-12'>
+                  <h6 class='heading-small mb-0'>
+                    Conversations :
+                  </h6>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div class="col-xl-4 col-lg-6">
-            <div class="card card-stats mb-4 mb-xl-0">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Likes</h5>
-                    <span class="h2 font-weight-bold mb-0"><?php echo $user['num_likes']; ?></span>
-                  </div>
+            <div class='card-body bg-secondary border-0'>
+              <div class='p-3'>
+                <?php
+                  echo $message_obj->getConvers();
+                ?>
+              
+              <div style="text-align: center;">
+                <a href="messages.php?u=new" class="btn btn-lg btn-primary mt-1 mb--2" >
+                  <span class="btn-inner--text" style="color: #fff;">
+                    New Message
+                  </span>
+                </a></div>
 
-                  <div class="col-auto">
-                    <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                      <i class="fas fa-heart"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-4 col-lg-6">
-            <div class="card card-stats mb-4 mb-xl-0">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Posts</h5>
-                    <span class="h2 font-weight-bold mb-0"><?php echo $user['num_posts']; ?></span>
-                  </div>
-
-                  <div class="col-auto">
-                    <div class="icon icon-shape bg-info text-white rounded-circle shadow">
-                      <i class="fas fa-comments"></i>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
 
-  <?php
-    if($user_to != "new") {
-      echo " 
-        <div class='container mt-5 mb-5'>
-          <div class='row justify-content-md-center'>
-            <div class='col-10'>
-              <div class='card bg-secondary shadow'>
-                <div class='card-header border-0'>
-                  <div class='row align-items-center'>
-                    <div class='col-12'>
-                      <h6 class='heading-small mb-0'>
-                        You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a>
-                      </h6>
-                    </div>
-                  </div>
-                </div>
-
-                <div class='card-body border-0'>
-                  <div class='p-3'>
+        <?php
+          if($user_to != "new") {
+            echo " 
+              <div class='col-xl-8 order-xl-1'>
+                <div class='card bg-secondary shadow'>
+                  <div class='card-header bg-white border-0'>
                     <div class='row align-items-center'>
-                      <div class='loaded_messages' id='scroll_messages'>" .
-                        $message_obj->getMessages($user_to) . "
+                      <div class='col-8'>
+                        <h6 class='heading-small mb-0'>
+                          You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a>
+                        </h6>
                       </div>
                     </div>
                   </div>
-                </div>
+
+                  <div class='card-body border-0'>
+                    <div class='p-3'>
+                      <div class='row align-items-center'>
+                        <div class='loaded_messages' id='scroll_messages'>" .
+                          $message_obj->getMessages($user_to) . "
+                        </div>
+                      </div>
+                    </div>
+                  </div>
               ";
             } else {
               echo "
-                <div class='container mt-5'>
-                  <div class='col'>
-                    <div class='card bg-secondary shadow'>
-                      <div class='card-header border-0'>
-                        <div class='row align-items-center'>
-                          <div class='col-12'>
-                            <h6 class='heading-small mb-0'>
-                              New Message
-                            </h6>
-                          </div>
+                <div class='col-xl-8 order-xl-1'>
+                  <div class='card bg-secondary shadow'>
+                    <div class='card-header bg-white border-0'>
+                      <div class='row align-items-center'>
+                        <div class='col-8'>
+                          <h6 class='heading-small mb-0'>
+                            New Message
+                          </h6>
                         </div>
                       </div>
                     </div>
@@ -153,12 +114,12 @@ if(isset($_POST['post_msg'])) {
                   echo 
                     "<div class='row justify-content-md-center'>
                       <div class='pl-lg-5 ml--2 col-10'>
-                        <div class='form-group mr-4'>
+                        <div class='form-group mr-4 mt--3'>
                           <textarea rows='3' class='form-control form-control-alternative' name='msg_body' placeholder='Write your message ...'></textarea>
                         </div>
                       </div>
 
-                      <div class='col-2 mt-4'>
+                      <div class='col-2 mt-2'>
                         <button type='submit' name='post_msg' class='btn btn-lg btn-primary btn-icon mb-4'>
                           <span class='btn-inner--icon'>
                             <i class='fas fa-paper-plane'></i>
