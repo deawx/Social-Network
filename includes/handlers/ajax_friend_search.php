76f4bb6eec58bdc5c4c366299a6ed0a44ff2a0f5
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-21 22:57:26 +0100

    Fix Query & add basic style of user search

--- includes/handlers/ajax_friend_search.php
+++ includes/handlers/ajax_friend_search.php
@@ -19,19 +19,38 @@ if(strpos($query, "_") !== false) {
                                        LIMIT 8");
 } else {
   $usersReturned = mysqli_query($con, "SELECT * FROM users 
-                                       WHERE (first_name LIKE '%$names[0]%' AND last_name LIKE '%$names[0]') 
+                                       WHERE (first_name LIKE '%$names[0]%' OR last_name LIKE '%$names[0]') 
                                        AND user_closed='no' 
                                        LIMIT 8");
 }
 
 if($query != "") {
   while($row = mysqli_fetch_array($usersReturned)) {
-    $user = new User($this->con, $userLoggedIn);
+    $user = new User($con, $userLoggedIn);
 
     if($row['username'] != $userLoggedIn) {
       $mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
     } else {
       $mutual_friends = "";
+    }
+
+    if($user->isFriend($row['username'])) {
+      echo "
+        <div class='resultDisplay'>
+          <a href='messages.php?u='" . $row['username'] . "' style='color: red;'>
+            <div class='LiveSearchProfilePic'>
+              <img src='" . $row['profile_pic'] . "' />
+            </div>
+
+            <div class='LiveSearchText'>"
+              . $row['first_name'] . " " . $row['last_name'] .
+            "</div>
+
+            <p>" . $row['username'] . "</p>
+            <p id='grey'>" . $mutual_friends . "</p>
+          </a>
+        </div>
+      ";
     }
   }
 }
