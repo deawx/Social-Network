c45c9dd7c5c9105ee3374f39e866ba54e53f297c
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-22 10:49:24 +0100

    Add getDropdownData() func

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -3,3 +3,31 @@ function getUser(value, user) {
     $(".results").html(data);
   });
 }
+
+function getDropdownData(user, type) {
+  if($(".dropdown-body").css("height") == "0px") {
+    var pageName;
+    if(type == 'notification') {
+
+    } else if(type == 'message') {
+      pageName = "ajax_load_messages.php";
+      $("span").remove("#unread_message");
+    }
+
+    var ajaxreq = $.ajax({
+      url: "includes/handlers/" + pageName,
+      type: "POST",
+      data: "page=1$user=" + user,
+      cache: false,
+
+      success: function(response) {
+        $(".dropdown-body").html(response);
+        $(".dropdown-body").css({"padding":"0px", "height":"280px"});
+        $("#dropdown_data_type").val(type);
+      }
+    });
+  } else {
+    $(".dropdown-body").html("");
+    $(".dropdown-body").css({"padding":"0px", "height":"0px"}); 
+  }
+}
