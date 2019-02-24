77f037f9e8a93ff6a41880be1a400a15da096232
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-24 14:09:09 +0100

    Add insertNotification() func

--- includes/classes/Notification.php
+++ includes/classes/Notification.php
@@ -8,3 +8,44 @@ class Notification {
     $this->con = $con;
     $this->user_obj = new User($con, $user);
   }
+
+  public function getUnreadNumber() {
+    $userLoggedIn = $this->user_obj->getUsername();
+    $query = mysqli_query($this->con, "SELECT * FROM notifications WHERE (viewed='no' AND user_to='$userLoggedIn')");
+    return mysqli_num_rows($query);
+  }
+
+  public function insertNotification($post_id, $user_to, $type) {
+    $userLoggedIn = $this->user_obj->getUsername();
+    $userLoggedInName = $this->user_obj->getFirstAndLastName();
+    $date_time = ("Y-m-d H:m:s");
+
+    switch($type) {
+      case 'comment':
+        $message = $userLoggedInName . " commented on your post";
+        break;
+
+      case 'like':
+        $message = $userLoggedInName . " like your post";
+        break;
+
+      case 'profile':
+        $message = $userLoggedInName . " posted on your profile";
+        break;
+
+      case 'comment_non_owner':
+        $message = $userLoggedInName . " commented on a post you commented on";
+        break;
+
+      case 'profile_comment':
+        $message = $userLoggedInName . " commented on your profile post";
+        break;
+    }
+
+    $link = "post.php?id=" . $post_id;
+    $insert_query = mysqli_query($this->con, "INSERT INTO notifications 
+                                              VALUES('', '$user_to', '$userLoggedIn', '$message', '$link', '$date_time', 'no', 'no')");
+  }
+}
+
+?>
