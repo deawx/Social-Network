398a604ff71f7d50cff1e5ff96fd879a25b8dbd4
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2019-02-24 14:59:51 +0100

    Fix Notification (argument)

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -35,7 +35,7 @@ class Post {
 
          // INSERT NOTIFICATION
          if($user_to != 'none') {
-           $notification = new Notification($this->con, $userLoggedIn);
+           $notification = new Notification($this->con, $added_by);
            $notification->insertNotification($returned_id, $user_to, 'profile_post');
          }
 
